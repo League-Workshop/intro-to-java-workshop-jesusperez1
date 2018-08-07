1454e5879a8f123fd640cf86d5e4fa4e653638c3
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:56:09 -0700

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot Guapo=new Robot();
+	Guapo.changeRobot("https://www.deere.com/assets/images/region-4/products/tractors/row-crop-tractors/6family-subgroup/7230r-r4d014778.jpg");
+Guapo.setSpeed(80);
+Guapo.setRandomPenColor();
+	Guapo.penDown();
+	int sides=45;
+			int angle=360/sides;
+		for(int i=0;i<sides;i++) {
+			Guapo.move(45);
+		Guapo.turn(angle);
+		}
+			
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,25 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot Guapo=new Robot();
 		
 		// 5. Set your robot's pen down 
-		
+		Guapo.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		Guapo.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		int sides=7;
+		int angle=360/sides;	
+		for (int i=0; i<75;i++) {
 			// 7. Change the pen color to random
-		
+					Guapo.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+					Guapo.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
+						Guapo.turn(360/sides);				
 		
 			// 8. Set the pen width to i
-			
+						Guapo.setPenWidth(i);
+			}
 	}
 }

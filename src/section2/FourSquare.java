25996976738c653ea9b9c96640f130f882215430
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 11:56:09 -0700

    Day 3

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,30 +4,35 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
+	Robot Bob = new Robot();
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		Bob.setSpeed(100);
 		// 5. Set the pen width to 5
-
+		Bob.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
-
+		for(int i=0; i<4; i++) {
+		// 7. Set the pen color to random
+			Bob.setRandomPenColor();
+		// 1. Call the drawSquare() method
+		drawSquare();
+		// 8. Turn the robot 90 degrees to the right
+		Bob.turn(90);
+		}
 	}
 
-	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		Bob.penDown();
+		for (int i = 0; i < 4; i++) {
+			Bob.move(100);
+			Bob.turn(360 / 4);
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -35,6 +40,3 @@ public class FourSquare {
 	}
 
 }
-
-
-
--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -3,37 +3,46 @@ package section2;
 import org.jointheleague.graphical.robot.Robot;
 
 public class TriangleShell {
-
+	int length = 50;
 	// 1. Create a new Robot
+	Robot Guapo = new Robot();
 
-	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		// 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
+		Guapo.setSpeed(100);
 
 		// 4. make a variable to hold the length of the triangle and set it to 50
 
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
-
+		for (int i = 0; i < 60; i++) {
 			// 9. Change the color of the pen to a random color
-	
+			Guapo.setRandomPenColor();
 			// 8. Increase the length variable by 10
-	
+			length = length + 10;
+
 			// 5. call your drawTriangle() method using your length variable
-	
+			drawTriangle(length);
+
 			// 10. Turn the robot 6 degrees to the right
-
+			Guapo.turn(6);
+		}
 	}
 
-	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
+	/*
+	 * 2. fill in the method below to draw a triangle. Use the length variable when
+	 * you call move().
+	 */
 	private void drawTriangle(int length) {
-		
-		
-		
+		Guapo.penDown();
+		for (int i = 0; i < 3; i++) {
+			Guapo.move(length);
+			Guapo.turn(360 / 3);
+		}
+
 	}
 
-	
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
